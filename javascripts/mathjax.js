714186cd4586be3a55b13aca88781c9696449d0f
craquinterogo <7codev@gmail.com>
2026-02-14 20:33:31 +0100

    fix: recurrent loop that broke the numeration

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -6,32 +6,17 @@ window.MathJax = {
     processEnvironments: true,
     tags: 'ams',  // Enable equation numbering (ams style)
     tagSide: 'right',  // Position numbers on the right
-    tagIndent: '0.8em',  // Indentation for equation numbers
-    packages: {'[+]': ['color', 'html']},  // Load color and html packages
-    macros: {
-      label: ['\\label{#1}', 1],
-      ref: ['\\ref{#1}', 1],
-      eqref: ['\\eqref{#1}', 1]
-    }
+    tagIndent: '0.8em'  // Indentation for equation numbers
   },
   options: {
     ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex",
-    enableAssistiveMml: true
-  },
-  loader: {
-    load: ['[tex]/color', '[tex]/html', '[tex]/ams']
-  },
-  startup: {
-    ready() {
-      MathJax.startup.defaultReady();
-      MathJax.startup.promise.then(() => {
-        console.log('MathJax initial typesetting complete');
-      });
-    }
+    processHtmlClass: "arithmatex"
   }
 };
 
-document$.subscribe(() => { 
-  MathJax.typesetPromise()
-})
+// For Material for MkDocs instant loading
+if (typeof document$ !== 'undefined') {
+  document$.subscribe(() => { 
+    MathJax.typesetPromise()
+  })
+}
