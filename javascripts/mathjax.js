dc251933da189fd5e3faf90352d5780f58ca8edc
craquinterogo <7codev@gmail.com>
2026-02-14 20:26:39 +0100

    feat: update of personal references for public

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -3,11 +3,18 @@ window.MathJax = {
     inlineMath: [["\\(", "\\)"]],
     displayMath: [["\\[", "\\]"]],
     processEscapes: true,
-    processEnvironments: true
+    processEnvironments: true,
+    tags: 'ams',  // Enable equation numbering (ams style)
+    tagSide: 'right',  // Position numbers on the right
+    tagIndent: '0.8em',  // Indentation for equation numbers
+    packages: {'[+]': ['color', 'html']}  // Load color and html packages
   },
   options: {
     ignoreHtmlClass: ".*|",
     processHtmlClass: "arithmatex"
+  },
+  loader: {
+    load: ['[tex]/color', '[tex]/html']
   }
 };
 
