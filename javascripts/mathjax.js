d701a620545d03e004405fd096d4299ca8204605
craquinterogo <7codev@gmail.com>
2026-02-14 20:30:00 +0100

    fix: numeration of formulas

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -7,14 +7,28 @@ window.MathJax = {
     tags: 'ams',  // Enable equation numbering (ams style)
     tagSide: 'right',  // Position numbers on the right
     tagIndent: '0.8em',  // Indentation for equation numbers
-    packages: {'[+]': ['color', 'html']}  // Load color and html packages
+    packages: {'[+]': ['color', 'html']},  // Load color and html packages
+    macros: {
+      label: ['\\label{#1}', 1],
+      ref: ['\\ref{#1}', 1],
+      eqref: ['\\eqref{#1}', 1]
+    }
   },
   options: {
     ignoreHtmlClass: ".*|",
-    processHtmlClass: "arithmatex"
+    processHtmlClass: "arithmatex",
+    enableAssistiveMml: true
   },
   loader: {
-    load: ['[tex]/color', '[tex]/html']
+    load: ['[tex]/color', '[tex]/html', '[tex]/ams']
+  },
+  startup: {
+    ready() {
+      MathJax.startup.defaultReady();
+      MathJax.startup.promise.then(() => {
+        console.log('MathJax initial typesetting complete');
+      });
+    }
   }
 };
 
